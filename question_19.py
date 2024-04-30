7c8d3a20e8102dd2332b1aeebe38add1f2f6ded2
JayChavda0020 <jaychavdam0020@gmail.com>
2024-04-30 13:25:26 +0530

    30-04-2024

--- question_19.py
+++ question_19.py
@@ -1,2 +1,18 @@
 # How Do You Handle Exceptions With Try/Except/Finally In Python ? Explain with coding snippets. 
 
+
+def divide(x, y): 
+	try: 
+		result = x // y 
+	except ZeroDivisionError: 
+		print("Sorry ! You are dividing by zero ") 
+	else:
+		print("Yeah ! Your answer is :", result) 
+	finally: 
+		# this block is always executed 
+		# regardless of exception generation. 
+		print('This is always executed') 
+
+# Look at parameters and note the working of Program 
+divide(3, 2) 
+divide(3, 0)
--- question_29.py
+++ question_29.py
@@ -1,2 +1,3 @@
 # What relationship is appropriate for Student and Person?
 
+# student is derived class from person (parent class). because every student is person but every person is not student
